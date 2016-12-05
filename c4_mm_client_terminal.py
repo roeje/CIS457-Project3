ba01c252a365bd399aa5610ccd12688878b3ce64
Ben Commet <bencommet@gmail.com>
2016-12-05 13:08:50 -0500

    added command line stuff

--- c4_mm_client_terminal.py
+++ c4_mm_client_terminal.py
@@ -5,7 +5,7 @@ from c4_gui import menu_gui
 import c4_client as c
 import c4_server as s
 
-
+server_data = None
 def main():
     server_data = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object
     server_msg = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,7 +26,40 @@ def main():
     print " Client2 received data:", data
 
 def startup():
-    print
+    commandNotFound = True
+    while commandNotFound:
+        command = raw_input("Type 'create' to create a game or 'join' to join a game")
+        if command.lower() == "create":
+            start_server()
+            commandNotFound = False
+        elif command.lower() == "join":
+            list_games()
+
+def list_games():
+    game_list = server_data.recieve(1024)
+
+    count = 1
+    for game in game_list:
+        print "Game No: ", count, "  Username: ", game[0], "  IP Adress: ", game[1]
+
+    validGame = False
+    selected_game = None
+    while not validGame:
+        gameNumString = raw_input("Enter the number of the game you wish to join.")
+        try:
+            gameNum = int(gameNumString)
+        except ValueError:
+            print 'Please enter an integer'
+
+        gameNum = gameNum - 1
+        if gameNum >= 0 and gameNum < game_list.length:
+            selected_game = game_list[gameNum]
+            validGame = True
+        else:
+            print "Please choose a number that is in the list of games."
+
+    create_client_game(selected_game[1])
+            
 
 def start_server():
     print '******************** Starting C4 Server ********************\n'
